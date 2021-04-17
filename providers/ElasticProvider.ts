2b4da70ee26192e3f283aa5c3e959c182dbc16aa
Eric Ellingson <god@eric.wtf>
2021-04-17 14:18:09 -0700

    try removing imports

--- providers/ElasticProvider.ts
+++ providers/ElasticProvider.ts
@@ -1,23 +1,24 @@
-import { ApplicationContract } from "@ioc:Adonis/Core/Application";
-import { Client, ClientOptions } from "@elastic/elasticsearch";
-import { AdonisElasticClient } from "../src/AdonisElasticClient";
+// import { ApplicationContract } from "@ioc:Adonis/Core/Application";
+// import { Client, ClientOptions } from "@elastic/elasticsearch";
+// import { AdonisElasticClient } from "../src/AdonisElasticClient";
 
 /**
  * Provider to register shield middleware
  */
 export default class ElasticProvider {
-  constructor(protected app: ApplicationContract) {}
+  constructor(protected app) {}
   public static needsApplication = true;
 
   public register() {
     this.app.container.singleton("Elastic", () => {
-      const Env = this.app.container.resolveBinding("Adonis/Core/Env");
-      const elasticUrl: string = Env.get("ELASTIC_SEARCH_URL");
-      const client: AdonisElasticClient = new AdonisElasticClient({
-        node: elasticUrl,
-      });
+      // const Env = this.app.container.resolveBinding("Adonis/Core/Env");
+      // const elasticUrl: string = Env.get("ELASTIC_SEARCH_URL");
+      // const client: AdonisElasticClient = new AdonisElasticClient({
+      //   node: elasticUrl,
+      // });
 
-      return client;
+      // return client;
+      return {};
     });
   }
 
